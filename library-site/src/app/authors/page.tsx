59c327c154cb334a7ed2bf9e2d025103a3728319
JoachimDieu <95349827+JoachimDieu@users.noreply.github.com>
2023-11-07 16:09:10 +0100

    resolve page authors

--- library-site/src/app/authors/page.tsx
+++ library-site/src/app/authors/page.tsx
@@ -9,22 +9,30 @@ const AuthorsPage: FC = (): ReactElement => {
   const { useListAuthors } = useAuthorsProviders();
   const { authors, authorsLoad } = useListAuthors();
   const [filterByName, setFilterByName] = useState<string>('');
-  const [filterByGenre, setFilterByGenre] = useState<string>('');
 
   useEffect(() => authorsLoad);
-
+  
   return (
     <>
-
-      <div className="grid grid-cols-4 justify-items-center">
-        {authors
-          .filter((author) =>
-            author.lastName.toLowerCase().includes(filterByName.toLowerCase()),
-          )
-          .map((author) => (
-            <AuthorCard key={author.id} author={author} />
-          ))}
-      </div>
+        <label htmlFor="filter" className="m-2">
+            Filtrer par auteur : &nbsp;
+            <input
+            className="border border-black rounded-md"
+            type="text"
+            onChange={(e): void => {
+                setFilterByName(e.target.value);
+            }}
+            />
+        </label>
+        <div className="grid grid-cols-4 justify-items-center">
+            {authors
+            .filter((author) =>
+                `${author.firstName} ${author.lastName}`.toLowerCase().includes(filterByName.toLowerCase())
+            )
+            .map((author) => (
+                <AuthorCard key={author.id} author={author} />
+            ))}
+        </div>
     </>
   );
 };
